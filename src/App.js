c888231c983225a473d8943a7aaddf5d092b5b36
Daniel Castilho <dan.castilho@gmail.com>
2021-05-22 20:36:43 -0400

    Card Component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
 		<div className="App">
 			<h1>Execício React-Redux (Simples)</h1>
 			<div className="linha">
-				<Card></Card>
+				<Card title="Card 1" red>x</Card>
+        <Card title="Card 2" green>x</Card>
 			</div>
 			<div className="linha">
-				<Card></Card>
+				<Card title="Card 3" blue>y</Card>
+        <Card title="Card 4" purple>y</Card>
 			</div>
 		</div>
 	);
