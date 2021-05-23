98ec7ec804ca89ef00f7e84b92bf3b744e91d025
Daniel Castilho <dan.castilho@gmail.com>
2021-05-22 21:23:00 -0400

    Implementation without Redeux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import "./App.css";
 
 import Intervalo from "./components/Intervalo";
@@ -6,20 +7,24 @@ import Soma from "./components/Soma";
 import Sorteio from "./components/Sorteio";
 
 function App() {
+	const [min, setMin] = useState(1);
+	const [max, setMax] = useState(10);
+
+
 	return (
 		<div className="App">
 			<h1>Execício React-Redux (Simples)</h1>
 			<div className="linha">
-				<Intervalo></Intervalo>
+				<Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}/>
 			</div>
 			<div className="linha">
-				<Media title="Card 2" green>
+				<Media min={min} max={max} green>
 					x
 				</Media>
-				<Soma title="Card 3" blue>
+				<Soma min={min} max={max} blue>
 					y
 				</Soma>
-				<Sorteio title="Card 4" purple>
+				<Sorteio min={min} max={max} purple>
 					y
 				</Sorteio>
 			</div>
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Card from "./Card";
 
 export default (props) => {
+	const { min, max } = props;
+
 	return (
 		<Card title="Soma dos Números" blue>
 			<div>
 				<span>
 					<span>Resultado: </span>
-					<strong>{props.soma}</strong>
+					<strong>{min + max}</strong>
 				</span>
 			</div>
 		</Card>
