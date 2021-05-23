5629211a3a22b922063fb8f89f2e7342135ca4eb
Daniel Castilho <dan.castilho@gmail.com>
2021-05-22 20:51:21 -0400

    Other Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Execício React-Redux (Simples)</h1>
 			<div className="linha">
-				<Card title="Card 1" red>x</Card>
-        <Card title="Card 2" green>x</Card>
+				<Intervalo></Intervalo>
 			</div>
 			<div className="linha">
+      <Card title="Card 2" green>x</Card>
 				<Card title="Card 3" blue>y</Card>
         <Card title="Card 4" purple>y</Card>
 			</div>
